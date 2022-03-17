62585ed829bcc2a28ecf388ba0089d5015a098f4
Saleh Yassin <yass0016@gmail.com>
2022-03-16 21:06:27 -0400

    Updates

--- main.js
+++ main.js
@@ -5204,8 +5204,12 @@ var API = /** @class */ (function () {
     API.validateToken = function (token) { return api_awaiter(void 0, void 0, void 0, function () {
         var result;
         return api_generator(_a, function (_b) {
-            result = httpGet(End_Points.VALIDATE_KEY, token);
-            return [2 /*return*/, result];
+            switch (_b.label) {
+                case 0: return [4 /*yield*/, httpGet(End_Points.VALIDATE_KEY, token)];
+                case 1:
+                    result = _b.sent();
+                    return [2 /*return*/, result];
+            }
         });
     }); };
     /**
@@ -5287,6 +5291,42 @@ var __assign = (undefined && undefined.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var Login_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var Login_generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 
@@ -5326,22 +5366,29 @@ var Login = function () {
     /**
      * Login function
      */
-    var login = function () {
-        // request the validate end point to check if token is valid
-        var res = API.validateToken(textFieldRef.current.value);
-        if (res.detail) {
-            api.event.emit(api.eventNames.EVENT_SNACKBAR_OPEN, mapId, {
-                message: {
-                    type: 'key',
-                    value: res.detail,
-                    params: [],
-                },
-            });
-        }
-        else {
-            state.auth.saveApiKey(textFieldRef.current.value);
-        }
-    };
+    var login = function () { return Login_awaiter(void 0, void 0, void 0, function () {
+        var res;
+        return Login_generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, API.validateToken(textFieldRef.current.value)];
+                case 1:
+                    res = (_a.sent());
+                    if (res.detail) {
+                        api.event.emit(api.eventNames.EVENT_SNACKBAR_OPEN, mapId, {
+                            message: {
+                                type: 'key',
+                                value: res.detail,
+                                params: [],
+                            },
+                        });
+                    }
+                    else {
+                        state.auth.saveApiKey(textFieldRef.current.value);
+                    }
+                    return [2 /*return*/];
+            }
+        });
+    }); };
     return ((0,jsx_runtime.jsxs)("div", __assign({ className: classes.loginContainer }, { children: [(0,jsx_runtime.jsx)(TextField, { id: "outlined-basic", label: "API Key Token", variant: "outlined", inputRef: textFieldRef, className: classes.loginTextInput }), (0,jsx_runtime.jsx)(Button, __assign({ tooltip: "Login", tooltipPlacement: "right", className: classes.loginBtn, variant: "contained", type: "text", onClick: function () { return login(); } }, { children: (0,jsx_runtime.jsx)("div", __assign({ className: classes.loginBtnText }, { children: "Login" })) }))] })));
 };
 
